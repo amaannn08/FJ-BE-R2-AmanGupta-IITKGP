30179f74c532be20c83e01948deb3083928e0b2d
linearmusic <amaannn.gupta.08@gmail.com>
2026-02-28 22:23:06 +0530

    Fixed some changes in index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,15 +3,13 @@ import dotenv from "dotenv"
 import cors from "cors"
 import {connectDB} from "./config/db"
 import authRoutes from "./routes/authRoutes";
+
 dotenv.config();
 const app = express()
-
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
-
-
+const PORT = process.env.PORT || 5000;
 
 app.use("/auth", authRoutes);
 
