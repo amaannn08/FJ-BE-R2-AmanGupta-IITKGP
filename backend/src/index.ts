19212134793ed411985cdb996ddd214377317484
linearmusic <amaannn.gupta.08@gmail.com>
2026-02-28 21:06:01 +0530

    Created some important files

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,15 @@
-console.log("Initialised the Repo")
+import express from "express"
+const app = express()
+
+
+app.post("/user",function(req,res){
+    
+})
+
+try {
+    app.listen(5000,()=>{
+        console.log("Server Running at PORT 5000")
+    });
+} catch (error) {
+    console.log("Error Running the Server")
+}
